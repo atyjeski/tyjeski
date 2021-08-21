8add5bacb7c93f7aa4133682503e7d44e7f1361c
atyjeski <atyjeski@gmail.com>
2021-08-21 19:03:37 -0400

    Separate the portfolio pages

--- backend/tyjeski/urls.py
+++ backend/tyjeski/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('portfolio', views.portfolio, name='portfolio')
+    path('portfolio-01', views.portfolio_01, name='portfolio-01'),
+    path('portfolio-02', views.portfolio_02, name='portfolio-02'),
+    path('portfolio-03', views.portfolio_03, name='portfolio-03'),
+    path('portfolio-04', views.portfolio_04, name='portfolio-04')
 ]
--- backend/tyjeski/views.py
+++ backend/tyjeski/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-def portfolio(request):
-    return render(request, 'portfolio-item.html')
+def portfolio_01(request):
+    return render(request, 'portfolio-item-01.html')
+
+def portfolio_02(request):
+    return render(request, 'portfolio-item-02.html')
+
+def portfolio_03(request):
+    return render(request, 'portfolio-item-03.html')
+
+def portfolio_04(request):
+    return render(request, 'portfolio-item-04.html')
